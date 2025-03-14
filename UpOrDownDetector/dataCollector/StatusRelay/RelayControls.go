0da74c7f9be75d53b95fc971c376b15f4b0e3a6c
Skvidich <skvidich0106@gmail.com>
2025-03-14 13:13:31 +0300

    refactor(data-collector)  enforce immutability for Relay configuration - Remove runtime modification methods:   - SetLogState()   - SetResendState() - Apply logging/resend settings via functional wrappers

--- UpOrDownDetector/dataCollector/StatusRelay/RelayControls.go
+++ UpOrDownDetector/dataCollector/StatusRelay/RelayControls.go
@@ -1,29 +1,19 @@
 package StatusRelay
 
-func (rel *StatusRelay) SetLogState(st bool) {
-	rel.muLog.Lock()
-	defer rel.muLog.Unlock()
-	rel.isLog = st
-}
-
 func (rel *StatusRelay) GetLogState() bool {
-	rel.muLog.Lock()
-	defer rel.muLog.Unlock()
+
 	return rel.isLog
 }
 
-func (rel *StatusRelay) SetResendState(st bool) {
-	rel.muResend.Lock()
-	defer rel.muResend.Unlock()
-	rel.isResend = st
-}
-
 func (rel *StatusRelay) GetResendState() bool {
-	rel.muResend.Lock()
-	defer rel.muResend.Unlock()
+
 	return rel.isResend
 }
 
 func (rel *StatusRelay) Run() {
 	go rel.inputProcess()
+}
+
+func (rel *StatusRelay) Close() {
+	<-rel.endChan
 }
