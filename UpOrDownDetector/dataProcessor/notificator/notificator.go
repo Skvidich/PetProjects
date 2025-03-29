c46f5e371aed4ef7928278c3e9bd8ea0019c409a
Skvidich <skvidich0106@gmail.com>
2025-03-29 20:22:28 +0300

    feat(data-process) add DbController, Notificator entities
    - Add DbController interface with SaveOverview(), SaveComponent(), SaveIncident() methods
    - Add NotificatorInterface with StartIncident() EndIncident() methods

---
+++ UpOrDownDetector/dataProcessor/notificator/notificator.go
@@ -0,0 +1,11 @@
+package notificator
+
+import (
+	"dataProcessor/common"
+	"time"
+)
+
+type Notificator interface {
+	StartIncident(name string, incdTime time.Time, comp common.Component) error
+	EndIncident(incd common.Incident) error
+}
