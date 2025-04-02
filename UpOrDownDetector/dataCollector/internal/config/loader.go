181c7eaf5408a056dbd8bbe817cc0eb8068bb5fb
Skvidich <skvidich0106@gmail.com>
2025-04-02 19:45:43 +0300

    feat(data-collector): update config loader
    - Introduce separate config structures for every entity
    - Add support of ini sections

--- UpOrDownDetector/dataCollector/internal/config/loader.go
+++ UpOrDownDetector/dataCollector/internal/config/loader.go
@@ -12,14 +12,35 @@ func LoadConfig(path string) (*Config, error) {
 		return nil, fmt.Errorf("can't load ini file %v", err)
 	}
 
-	cfg := &Config{
-		ErrLog:  "logs/error.log",
-		StatLog: "logs/status.log",
-	}
-	section := iniFile.Section("")
-	err = section.MapTo(cfg)
+	cfg := &Config{}
+	section := iniFile.Section("Logger")
+	err = section.MapTo(cfg.LoggerConfig)
 	if err != nil {
-		return nil, fmt.Errorf("config mapping failed: %w", err)
+		return nil, fmt.Errorf("config logger mapping failed: %w", err)
+	}
+
+	section = iniFile.Section("Producer")
+	err = section.MapTo(cfg.KafkaProducerConfig)
+	if err != nil {
+		return nil, fmt.Errorf("config producer mapping failed: %w", err)
+	}
+
+	section = iniFile.Section("Coordinator")
+	err = section.MapTo(cfg.CoordinatorConfig)
+	if err != nil {
+		return nil, fmt.Errorf("config coordinator mapping failed: %w", err)
+	}
+
+	section = iniFile.Section("Storage")
+	err = section.MapTo(cfg.StorageConfig)
+	if err != nil {
+		return nil, fmt.Errorf("config storage mapping failed: %w", err)
+	}
+
+	section = iniFile.Section("Relay")
+	err = section.MapTo(cfg.RelayConfig)
+	if err != nil {
+		return nil, fmt.Errorf("config relay mapping failed: %w", err)
 	}
 
 	return cfg, nil
--- UpOrDownDetector/dataCollector/internal/config/types.go
+++ UpOrDownDetector/dataCollector/internal/config/types.go
@@ -3,12 +3,33 @@ package config
 import "time"
 
 type Config struct {
-	ReqDelay     time.Duration `ini:"ReqDelay"`
-	Getters      []string      `ini:"Getters,omitempty"`
-	LogRelay     bool          `ini:"LogRelay"`
-	ResendRelay  bool          `ini:"ResendRelay"`
-	ErrLog       string        `ini:"ErrLog"`
-	StatLog      string        `ini:"StatLog"`
-	KafkaBrokers []string      `ini:"KafkaBrokers,omitempty"`
-	KafkaTopic   string        `ini:"KafkaTopic"`
+	CoordinatorConfig
+	RelayConfig
+	LoggerConfig
+	KafkaProducerConfig
+	StorageConfig
+}
+type CoordinatorConfig struct {
+	ReqDelay time.Duration `ini:"ReqDelay"`
+	Getters  []string      `ini:"Getters"`
+}
+
+type RelayConfig struct {
+	Save   bool `ini:"Save"`
+	Resend bool `ini:"Resend"`
+}
+
+type LoggerConfig struct {
+	ErrLog string `ini:"ErrLog"`
+}
+
+type KafkaProducerConfig struct {
+	Brokers []string `ini:"Brokers"`
+	Topic   string   `ini:"Topic"`
+}
+
+type StorageConfig struct {
+	DSN            string `ini:"DSN"`
+	ReportTable    string `ini:"ReportTable"`
+	ComponentTable string `ini:"ComponentTable"`
 }
