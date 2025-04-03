5407dc7af9fe60d6278d5f4e8bd8ce9f18229976
Skvidich <skvidich0106@gmail.com>
2025-04-03 12:25:13 +0300

    feat(data-processor): implement SQLStorage
    PostgreSQL database to store data. Implement Repository interface.

--- UpOrDownDetector/dataProcessor/internal/core/storage/postgres.go
+++ UpOrDownDetector/dataProcessor/internal/core/storage/postgres.go
@@ -2,31 +2,95 @@ package storage
 
 import (
 	"dataProcessor/pkg/models"
+	"database/sql"
+	"errors"
 	"fmt"
+	"strconv"
 	"time"
 )
 
-type SQLStore struct {
+type SQLStorage struct {
+	db             *sql.DB
+	reportTable    string
+	incidentTable  string
+	componentTable string
 }
 
-func NewSQLStore() *SQLStore {
-	return &SQLStore{}
+func NewSQLStorage(db *sql.DB, reportTable, incidentTable, componentTable string) (*SQLStorage, error) {
+	return &SQLStorage{
+		db:             db,
+		reportTable:    reportTable,
+		incidentTable:  incidentTable,
+		componentTable: componentTable,
+	}, nil
 }
 
-func (s *SQLStore) StoreReport(report *models.Report, metrics []models.ComponentMetrics) error {
-	fmt.Println("statistic saved")
-	fmt.Println(report.Name, " ", report.PeriodStart.String(), " ", report.PeriodEnd.String())
-	for _, m := range metrics {
-		fmt.Printf("Metric: %s - %s (%d)\n", m.Component, m.State, m.Count)
+func (s *SQLStorage) StoreReport(overview *models.Report, components []models.ComponentMetrics) error {
+
+	tx, err := s.db.Begin()
+	if err != nil {
+		return fmt.Errorf("can't start transaction %w", err)
 	}
+	defer func() {
+		if err := tx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone) {
+
+		}
+	}()
+
+	query := "INSERT INTO " + s.reportTable + " (service_name,start_time,end_time) VALUES ($1, $2, $3) RETURNING id"
+	result := tx.QueryRow(query, overview.Name, overview.PeriodStart.Format(time.DateTime), overview.PeriodEnd.Format(time.DateTime))
+
+	var reportId int64
+	if err = result.Scan(&reportId); err != nil {
+		return fmt.Errorf("can't read last inserted id %w", err)
+	}
+
+	query = "INSERT INTO " + s.componentTable + " (report_id,component_name,status,status_count) VALUES (" + strconv.FormatInt(reportId, 10) + ",$1,$2,$3)"
+	prep, err := tx.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("can't prepare query %w", err)
+	}
+	defer func() {
+		if err := prep.Close(); err != nil {
+
+		}
+	}()
+
+	for _, metric := range components {
+		_, err = prep.Exec(metric.Component, metric.State, metric.Count)
+		if err != nil {
+			return fmt.Errorf("can't execute query %w", err)
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("can't commit transaction %w", err)
+	}
+
 	return nil
 }
 
-func (s *SQLStore) StoreIncident(incident *models.ServiceIncident) error {
-	fmt.Printf("Recording incident: %s [%s] %s-%s\n",
-		incident.Name,
-		incident.Component,
-		incident.StartTime.Format(time.RFC1123),
-		incident.EndTime.Format(time.RFC1123))
+func (s *SQLStorage) StoreIncident(incident *models.ServiceIncident) error {
+
+	tx, err := s.db.Begin()
+	if err != nil {
+		return fmt.Errorf("can't start transaction %w", err)
+	}
+	defer func() {
+		if err := tx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone) {
+
+		}
+	}()
+
+	query := "INSERT INTO " + s.incidentTable + " (service_name,component_name,status,start_time,end_time) VALUES ($1, $2, $3,$4,$5)"
+	_, err = tx.Exec(query, incident.Name, incident.Component, incident.State, incident.StartTime, incident.EndTime)
+	if err != nil {
+		return fmt.Errorf("can't execute query %w", err)
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("can't commit transaction %w", err)
+	}
+
 	return nil
 }
