f1b7b1db83404ab66fdff6806bd3715a92e07a9e
Skvidich <skvidich0106@gmail.com>
2025-05-24 23:46:02 +0300

    fix(statusServer): remove table names as part of PostgresRepo structure

--- statusServer/internal/core/repository/postgres.go
+++ statusServer/internal/core/repository/postgres.go
@@ -3,29 +3,19 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"statusServer/pkg/models"
 	"time"
 )
 
 type PostgresRepo struct {
 	// TODO add comprehensive logic
-	db             *sql.DB
-	ReportTable    string
-	IncidentTable  string
-	ComponentTable string
+	db *sql.DB
 }
 
-func NewPostgresRepo(db *sql.DB,
-	ReportTable string,
-	IncidentTable string,
-	ComponentTable string) (StatusRepository, error) {
+func NewRepository(db *sql.DB) *PostgresRepo {
 	return &PostgresRepo{
-		db:             db,
-		ReportTable:    ReportTable,
-		IncidentTable:  IncidentTable,
-		ComponentTable: ComponentTable,
-	}, nil
+		db: db,
+	}
 }
 
 func (p *PostgresRepo) Close() error {
@@ -39,7 +29,7 @@ func (p *PostgresRepo) Close() error {
 func (p *PostgresRepo) GetMetricByID(ctx context.Context, id int) (*models.ComponentMetric, error) {
 
 	op := "GetMetricsByID"
-	query := fmt.Sprintf("SELECT id,report_id,component_name,status,status_count FROM %s WHERE id=$1", p.ComponentTable)
+	query := "SELECT id,report_id,component_name,status,status_count FROM component_metrics WHERE id=$1"
 
 	res := p.db.QueryRowContext(ctx, query, id)
 	if res.Err() != nil {
@@ -62,14 +52,14 @@ func (p *PostgresRepo) GetMetricByID(ctx context.Context, id int) (*models.Compo
 		}
 	}
 
-	return &metric, &Error{}
+	return &metric, nil
 
 }
 
 func (p *PostgresRepo) ListMetricsByReport(ctx context.Context, reportID int) ([]*models.ComponentMetric, error) {
 
 	op := "ListMetricsByReport"
-	query := fmt.Sprintf("SELECT id,report_id,component_name,status,status_count FROM %s WHERE report_id=$1", p.ComponentTable)
+	query := "SELECT id,report_id,component_name,status,status_count FROM component_metrics  WHERE report_id=$1"
 
 	rows, err := p.db.QueryContext(ctx, query, reportID)
 	if err != nil {
@@ -96,13 +86,13 @@ func (p *PostgresRepo) ListMetricsByReport(ctx context.Context, reportID int) ([
 		metrics = append(metrics, &metric)
 	}
 
-	return metrics, &Error{}
+	return metrics, nil
 }
 
 func (p *PostgresRepo) ListMetricsByComponent(ctx context.Context, comp string) ([]*models.ComponentMetric, error) {
 
 	op := "ListMetricsByComponent"
-	query := fmt.Sprintf("SELECT id,report_id,component_name,status,status_count FROM %s WHERE component_name=$1", p.ComponentTable)
+	query := "SELECT id,report_id,component_name,status,status_count FROM component_metrics  WHERE component_name=$1"
 
 	rows, err := p.db.QueryContext(ctx, query, comp)
 	if err != nil {
@@ -129,13 +119,13 @@ func (p *PostgresRepo) ListMetricsByComponent(ctx context.Context, comp string) 
 		metrics = append(metrics, &metric)
 	}
 
-	return metrics, &Error{}
+	return metrics, nil
 
 }
 
 func (p *PostgresRepo) ListMetricsByStatus(ctx context.Context, status string) ([]*models.ComponentMetric, error) {
 	op := "ListMetricsByStatus"
-	query := fmt.Sprintf("SELECT id,report_id,component_name,status,status_count FROM %s WHERE status=$1", p.ComponentTable)
+	query := "SELECT id,report_id,component_name,status,status_count FROM component_metrics  WHERE status=$1"
 
 	rows, err := p.db.QueryContext(ctx, query, status)
 	if err != nil {
@@ -162,13 +152,13 @@ func (p *PostgresRepo) ListMetricsByStatus(ctx context.Context, status string) (
 		metrics = append(metrics, &metric)
 	}
 
-	return metrics, &Error{}
+	return metrics, nil
 
 }
 
 func (p *PostgresRepo) ListMetricsByReportAndStatus(ctx context.Context, reportID int, status string) ([]*models.ComponentMetric, error) {
 	op := "ListMetricsByReportAndStatus"
-	query := fmt.Sprintf("SELECT id,report_id,component_name,status,status_count FROM %s WHERE status=$1 AND report_id=$2", p.ComponentTable)
+	query := "SELECT id,report_id,component_name,status,status_count FROM component_metrics  WHERE status=$1 AND report_id=$2"
 
 	rows, err := p.db.QueryContext(ctx, query, status, reportID)
 	if err != nil {
@@ -195,12 +185,12 @@ func (p *PostgresRepo) ListMetricsByReportAndStatus(ctx context.Context, reportI
 		metrics = append(metrics, &metric)
 	}
 
-	return metrics, &Error{}
+	return metrics, nil
 }
 
 func (p *PostgresRepo) CountMetricsByReport(ctx context.Context, reportID int) (int, error) {
 	op := "CountMetricsByReport"
-	query := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE report_id=$1", p.ComponentTable)
+	query := "SELECT COUNT(*) FROM component_metrics  WHERE report_id=$1"
 
 	res := p.db.QueryRowContext(ctx, query, reportID)
 	if res.Err() != nil {
@@ -223,7 +213,7 @@ func (p *PostgresRepo) CountMetricsByReport(ctx context.Context, reportID int) (
 		}
 	}
 
-	return metricCount, &Error{}
+	return metricCount, nil
 }
 
 // Incident interface
@@ -232,10 +222,8 @@ func (p *PostgresRepo) CountMetricsByReport(ctx context.Context, reportID int) (
 
 func (p *PostgresRepo) GetIncidentByID(ctx context.Context, id int) (*models.Incident, error) {
 	op := "GetIncidentByID"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, component_name, status, start_time, end_time FROM %s WHERE id=$1",
-		p.IncidentTable,
-	)
+	query := "SELECT id, service_name, component_name, status, start_time, end_time FROM incidents WHERE id=$1"
+
 	row := p.db.QueryRowContext(ctx, query, id)
 	if err := row.Err(); err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -244,15 +232,12 @@ func (p *PostgresRepo) GetIncidentByID(ctx context.Context, id int) (*models.Inc
 	if err := row.Scan(&inc.ID, &inc.ServiceName, &inc.ComponentName, &inc.Status, &inc.StartTime, &inc.EndTime); err != nil {
 		return nil, &Error{Err: err, Problem: "can't scan row", Method: op}
 	}
-	return &inc, &Error{}
+	return &inc, nil
 }
 
 func (p *PostgresRepo) ListIncidents(ctx context.Context, service, component string, from, to time.Time) ([]*models.Incident, error) {
 	op := "ListIncidents"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, component_name, status, start_time, end_time FROM %s WHERE service_name=$1 AND component_name=$2 AND start_time>=$3 AND end_time<=$4 ORDER BY id",
-		p.IncidentTable,
-	)
+	query := " SELECT id, service_name, component_name, status, start_time, end_time FROM incidents WHERE service_name=$1 AND component_name=$2 AND start_time>=$3 AND end_time<=$4 ORDER BY id"
 	rows, err := p.db.QueryContext(ctx, query, service, component, from, to)
 	if err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -266,15 +251,12 @@ func (p *PostgresRepo) ListIncidents(ctx context.Context, service, component str
 		}
 		list = append(list, &inc)
 	}
-	return list, &Error{}
+	return list, nil
 }
 
 func (p *PostgresRepo) ListIncidentsByService(ctx context.Context, service string, from, to time.Time) ([]*models.Incident, error) {
 	op := "ListIncidentsByService"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, component_name, status, start_time, end_time FROM %s WHERE service_name=$1 AND start_time>=$2 AND end_time<=$3 ORDER BY id",
-		p.IncidentTable,
-	)
+	query := "SELECT id, service_name, component_name, status, start_time, end_time FROM incidents WHERE service_name=$1 AND start_time>=$2 AND end_time<=$3 ORDER BY id"
 	rows, err := p.db.QueryContext(ctx, query, service, from, to)
 	if err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -288,15 +270,13 @@ func (p *PostgresRepo) ListIncidentsByService(ctx context.Context, service strin
 		}
 		list = append(list, &inc)
 	}
-	return list, &Error{}
+	return list, nil
 }
 
 func (p *PostgresRepo) ListIncidentsByComponent(ctx context.Context, component string, from, to time.Time) ([]*models.Incident, error) {
 	op := "ListIncidentsByComponent"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, component_name, status, start_time, end_time FROM %s WHERE component_name=$1 AND start_time>=$2 AND end_time<=$3 ORDER BY id",
-		p.IncidentTable,
-	)
+	query := "SELECT id, service_name, component_name, status, start_time, end_time FROM incidents WHERE component_name=$1 AND start_time>=$2 AND end_time<=$3 ORDER BY id"
+
 	rows, err := p.db.QueryContext(ctx, query, component, from, to)
 	if err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -310,15 +290,13 @@ func (p *PostgresRepo) ListIncidentsByComponent(ctx context.Context, component s
 		}
 		list = append(list, &inc)
 	}
-	return list, &Error{}
+	return list, nil
 }
 
 func (p *PostgresRepo) CountIncidentsByService(ctx context.Context, service string, from, to time.Time) (int, error) {
 	op := "CountIncidentsByService"
-	query := fmt.Sprintf(
-		"SELECT COUNT(*) FROM %s WHERE service_name=$1 AND start_time>=$2 AND end_time<=$3",
-		p.IncidentTable,
-	)
+	query := "SELECT COUNT(*) FROM incidents WHERE service_name=$1 AND start_time>=$2 AND end_time<=$3"
+
 	row := p.db.QueryRowContext(ctx, query, service, from, to)
 	if err := row.Err(); err != nil {
 		return 0, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -327,15 +305,13 @@ func (p *PostgresRepo) CountIncidentsByService(ctx context.Context, service stri
 	if err := row.Scan(&count); err != nil {
 		return 0, &Error{Err: err, Problem: "can't scan row", Method: op}
 	}
-	return count, &Error{}
+	return count, nil
 }
 
 func (p *PostgresRepo) CountIncidentsByComponent(ctx context.Context, component string, from, to time.Time) (int, error) {
 	op := "CountIncidentsByComponent"
-	query := fmt.Sprintf(
-		"SELECT COUNT(*) FROM %s WHERE component_name=$1 AND start_time>=$2 AND end_time<=$3",
-		p.IncidentTable,
-	)
+	query := "SELECT COUNT(*) FROM incidents WHERE component_name=$1 AND start_time>=$2 AND end_time<=$3"
+
 	row := p.db.QueryRowContext(ctx, query, component, from, to)
 	if err := row.Err(); err != nil {
 		return 0, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -344,7 +320,7 @@ func (p *PostgresRepo) CountIncidentsByComponent(ctx context.Context, component 
 	if err := row.Scan(&count); err != nil {
 		return 0, &Error{Err: err, Problem: "can't scan row", Method: op}
 	}
-	return count, &Error{}
+	return count, nil
 }
 
 // Report interface
@@ -353,10 +329,8 @@ func (p *PostgresRepo) CountIncidentsByComponent(ctx context.Context, component 
 
 func (p *PostgresRepo) GetReportByID(ctx context.Context, id int) (*models.Report, error) {
 	op := "GetReportByID"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, start_time, end_time FROM %s WHERE id=$1",
-		p.ReportTable,
-	)
+	query := "SELECT id, service_name, start_time, end_time FROM reports WHERE id=$1"
+
 	row := p.db.QueryRowContext(ctx, query, id)
 	if err := row.Err(); err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -365,21 +339,19 @@ func (p *PostgresRepo) GetReportByID(ctx context.Context, id int) (*models.Repor
 	if err := row.Scan(&r.ID, &r.ServiceName, &r.StartTime, &r.EndTime); err != nil {
 		return nil, &Error{Err: err, Problem: "can't scan row", Method: op}
 	}
-	return &r, &Error{}
+	return &r, nil
 }
 
 func (p *PostgresRepo) ListReports(ctx context.Context, offset, limit int) ([]*models.Report, error) {
 	op := "ListReports"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, start_time, end_time FROM %s ORDER BY id OFFSET $1 LIMIT $2",
-		p.ReportTable,
-	)
+	query := "SELECT id, service_name, start_time, end_time FROM reports ORDER BY id OFFSET $1 LIMIT $2"
+
 	rows, err := p.db.QueryContext(ctx, query, offset, limit)
 	if err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
 	}
 	defer rows.Close()
-	var list []*models.Report
+	list := make([]*models.Report, 0)
 	for rows.Next() {
 		var r models.Report
 		if err = rows.Scan(&r.ID, &r.ServiceName, &r.StartTime, &r.EndTime); err != nil {
@@ -387,15 +359,13 @@ func (p *PostgresRepo) ListReports(ctx context.Context, offset, limit int) ([]*m
 		}
 		list = append(list, &r)
 	}
-	return list, &Error{}
+	return list, nil
 }
 
 func (p *PostgresRepo) ListReportsByService(ctx context.Context, serviceName string, offset, limit int) ([]*models.Report, error) {
 	op := "ListReportsByService"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, start_time, end_time FROM %s WHERE service_name=$1 ORDER BY id OFFSET $2 LIMIT $3",
-		p.ReportTable,
-	)
+	query := "SELECT id, service_name, start_time, end_time FROM %s WHERE service_name=$1 ORDER BY id OFFSET $2 LIMIT $3"
+
 	rows, err := p.db.QueryContext(ctx, query, serviceName, offset, limit)
 	if err != nil {
 		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -409,18 +379,15 @@ func (p *PostgresRepo) ListReportsByService(ctx context.Context, serviceName str
 		}
 		list = append(list, &r)
 	}
-	return list, &Error{}
+	return list, nil
 }
 
 func (p *PostgresRepo) ListReportsByPeriod(ctx context.Context, from, to time.Time, offset, limit int) ([]*models.Report, error) {
 	op := "ListReportsByPeriod"
-	query := fmt.Sprintf(
-		"SELECT id, service_name, start_time, end_time FROM %s WHERE start_time>=$1 AND end_time<=$2 ORDER BY id OFFSET $3 LIMIT $4",
-		p.ReportTable,
-	)
+	query := "SELECT id, service_name, start_time, end_time FROM reports WHERE start_time>=$1 AND end_time<=$2 ORDER BY id OFFSET $3 LIMIT $4"
 	rows, err := p.db.QueryContext(ctx, query, from, to, offset, limit)
 	if err != nil {
-		return nil, &Error{Err: err, Problem: "can't execute query", Method: op}
+		return nil, &Error{Err: ErrNotFound, Problem: "can't execute query", Method: op}
 	}
 	defer rows.Close()
 	var list []*models.Report
@@ -431,12 +398,12 @@ func (p *PostgresRepo) ListReportsByPeriod(ctx context.Context, from, to time.Ti
 		}
 		list = append(list, &r)
 	}
-	return list, &Error{}
+	return list, nil
 }
 
 func (p *PostgresRepo) CountReports(ctx context.Context) (int, error) {
 	op := "CountReports"
-	query := fmt.Sprintf("SELECT COUNT(*) FROM %s", p.ReportTable)
+	query := "SELECT COUNT(*) FROM reports"
 	row := p.db.QueryRowContext(ctx, query)
 	if err := row.Err(); err != nil {
 		return 0, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -445,12 +412,12 @@ func (p *PostgresRepo) CountReports(ctx context.Context) (int, error) {
 	if err := row.Scan(&count); err != nil {
 		return 0, &Error{Err: err, Problem: "can't scan row", Method: op}
 	}
-	return count, &Error{}
+	return count, nil
 }
 
 func (p *PostgresRepo) CountReportsByService(ctx context.Context, serviceName string) (int, error) {
 	op := "CountReportsByService"
-	query := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE service_name=$1", p.ReportTable)
+	query := "SELECT COUNT(*) FROM reports WHERE service_name=$1"
 	row := p.db.QueryRowContext(ctx, query, serviceName)
 	if err := row.Err(); err != nil {
 		return 0, &Error{Err: err, Problem: "can't execute query", Method: op}
@@ -459,5 +426,5 @@ func (p *PostgresRepo) CountReportsByService(ctx context.Context, serviceName st
 	if err := row.Scan(&count); err != nil {
 		return 0, &Error{Err: err, Problem: "can't scan row", Method: op}
 	}
-	return count, &Error{}
+	return count, nil
 }
